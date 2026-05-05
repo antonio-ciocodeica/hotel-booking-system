767e23941125d64bf2e551340ccbc4337160dbab
Antonio770 <ciocodeicaantonio770@gmail.com>
2026-05-05 23:10:23 +0300

    feat(backend): add repositories

---
+++ backend/src/main/java/com/hotelbooking/backend/repositories/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.hotelbooking.backend.repositories;
+
+import com.hotelbooking.backend.domain.entities.RoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoomRepository extends JpaRepository<RoomEntity, UUID> {
+}
